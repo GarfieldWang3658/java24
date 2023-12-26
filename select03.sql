b7097fd7d7d4ef3cb14b3a97351fc453c5410627
GarfieldWang3658 <116738842+GarfieldWang3658@users.noreply.github.com>
2023-12-26 18:47:06 +0900

    Add files via upload

---
+++ select03.sql
@@ -0,0 +1,8 @@
+INSERT INTO `select03` (`id`, `name`, `chinese`, `english`, `math`) VALUES (4, 'shenjiayan', 83, 72, 91);
+INSERT INTO `select03` (`id`, `name`, `chinese`, `english`, `math`) VALUES (5, 'chengshenheng', 81, 76, 70);
+INSERT INTO `select03` (`id`, `name`, `chinese`, `english`, `math`) VALUES (7, 'aoki', 84, 65, 70);
+INSERT INTO `select03` (`id`, `name`, `chinese`, `english`, `math`) VALUES (1, 'wangyang', 89, 78, 90);
+INSERT INTO `select03` (`id`, `name`, `chinese`, `english`, `math`) VALUES (2, 'wangmiduo', 79, 68, 80);
+INSERT INTO `select03` (`id`, `name`, `chinese`, `english`, `math`) VALUES (4, 'shenjiayan', 83, 72, 91);
+INSERT INTO `select03` (`id`, `name`, `chinese`, `english`, `math`) VALUES (6, 'tanaka', 33, 22, 44);
+INSERT INTO `select03` (`id`, `name`, `chinese`, `english`, `math`) VALUES (7, 'aoki', 84, 65, 70);
